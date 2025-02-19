a3ab33218b2566bf6ee266b786502dab4e45b2bb
Mohammed Salih k <salihsha656@gmail.com>
2025-02-19 21:39:08 +0530

    first

---
+++ src/roomHandler.ts
@@ -0,0 +1,40 @@
+import { Socket } from "socket.io";
+import {v4 as uuidV4} from 'uuid';
+
+const rooms:Record<string,string[]>={}
+interface IRoomParams{
+    roomId:string;
+    peerId:string;
+}
+export const roomHandler=(socket:Socket)=>{
+    const createRoom=()=>{
+
+        console.log(`hii`)
+        const roomId=uuidV4();
+        rooms[roomId]=[];
+        socket.emit('room-created',{roomId})
+        console.log(`user create-room`)
+    }
+    const joinRoom=({roomId,peerId}:IRoomParams)=>{
+        if(rooms[roomId]){
+        console.log(`user join-room ${roomId}`);
+        rooms[roomId].push(peerId)
+        socket.join(roomId);
+        socket.emit('get-users',{
+            roomId,
+            participants:rooms[roomId],
+        })
+        socket.on('disconnect',()=>{
+            console.log(`user left the room`,peerId);
+            leaveRoom({roomId,peerId})
+        })
+    }
+    }  
+const leaveRoom=({roomId,peerId}:IRoomParams)=>{
+    rooms[roomId]=rooms[roomId].filter(id=>id!==peerId);
+    socket.to(roomId).emit('user-disconnected',peerId)
+}
+socket.on('create-room',createRoom); 
+socket.on('join-room',joinRoom);
+ 
+}
